d63754c87f47b13f4801a10ee3735e3124181c7c
Edgarbanzo <edgarzepedau@hotmail.com>
2023-05-19 21:07:24 -0600

    Carousel Flashcards

--- src/pages/landingPage/index.tsx
+++ src/pages/landingPage/index.tsx
@@ -6,133 +6,207 @@ import IArticle from "../../models/article";
 import Link from "next/link";
 import Footer from "@/layouts/footer/footer";
 
-const dummyData: IArticle[] = [
-  {
-    id: 0,
-    title: "Hola Edgar",
-    description: "soy un texto de prueba",
-    createdBy: "Edgar",
-    image:
-      "https://yca.org.ar/wp-content/uploads/sites/4/2019/06/perfil-avatar-hombre-icono-redondo_24640-14044.jpg",
-  },
-  {
-    id: 1,
-    title: "Ke pedo",
-    description: "soy un texto de prueba dos",
-    createdBy: "Edgar",
-    image:
-      "https://yca.org.ar/wp-content/uploads/sites/4/2019/06/perfil-avatar-hombre-icono-redondo_24640-14044.jpg",
-  },
-  {
-    id: 2,
-    title: "SS",
-    description: "soy un texto de prueba 3s",
-    createdBy: "IVAN",
-    image:
-      "https://yca.org.ar/wp-content/uploads/sites/4/2019/06/perfil-avatar-hombre-icono-redondo_24640-14044.jpg",
-  },
-];
-
 const DummyComponent = ({ data }: { data: IArticle[] }) => {
-  return (
-    <>
-      {data.map((article) => (
-        <article key={article.id} className="group w-1/5">
-          <div className="w-full max-w-md px-8 py-4 mt-16 bg-white rounded-lg shadow-lg dark:bg-gray-800">
-            <div className="flex justify-center -mt-16 md:justify-end">
-              <div className="object-cover w-20 h-20 border-2 border-blue-500 rounded-full dark:border-blue-400">
-                <Image
-                  src={article.image}
-                  alt="Avatar"
-                  height={36}
-                  width={36}
-                />
-              </div>
-            </div>
-            <h2 className="mt-2 text-xl font-semibold text-gray-800 dark:text-white md:mt-0">
-              {article.title}
-            </h2>
-            <p className="mt-2 text-sm text-gray-600 dark:text-gray-200">
-              {article.description}
-            </p>
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-            <div className="flex justify-end mt-4">
-              <a
-                href="#"
-                className="text-lg font-medium text-blue-600 dark:text-blue-300"
-                tabIndex={0}
-                role="link"
-              >
-                John Doe
-              </a>
-            </div>
-          </div>
-          <div className="p-2">
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % data.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide - 1 + data.length) % data.length);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setCurrentSlide(0); // Reset current slide on window resize
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // Adjust the data to show only 3 flashcards
+  const visibleData = data.slice(currentSlide, currentSlide + 3);
+
+  return (
+    <div className="carousel">
+      <div className="carousel-inner flex space-x-4">
+        {visibleData.map((article) => (
+          <article
+            key={article.id}
+            className="space-x-4 d-block w-100 grid-flow-row pt-6 relative block overflow-hidden rounded-lg border border-gray-100 sm:p-6 lg:p-8"
+          >
             <a href="#">
-              <h3 className="text-lg font-medium text-white">
-                {article.title}
-              </h3>
+              <span className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"></span>
+
+              <div className="sm:flex sm:justify-between sm:gap-4">
+                <div>
+                  <h3 className="text-lg font-bold text-white sm:text-xl">
+                    {article.title}
+                  </h3>
+
+                  <p className="mt-1 text-xs font-medium text-gray-600">
+                    {article.createdBy}
+                  </p>
+                </div>
+
+                <div className="hidden sm:block sm:shrink-0">
+                  <Image
+                    alt="Paul Clapton"
+                    src={article.image}
+                    width={64}
+                    height={64}
+                    className="h-16 w-16 rounded-lg object-cover shadow-sm"
+                  />
+                </div>
+              </div>
+
+              <div className="mt-4">
+                <p className="max-w-[40ch] text-sm text-gray-500">
+                  {article.description}
+                </p>
+              </div>
             </a>
-            <p className="mt-2 mb-8 text-sm text-ellipsis overflow-hidden ... white-space: nowrap w-auto leading-relaxed text-gray-500 line-clamp-3">
-              {article.description}
-              <span className="flow-root">{article.createdBy}</span>
-            </p>
-            <p className="mt-2 mb-8 text-sm text-ellipsis overflow-hidden ... white-space: nowrap w-auto leading-relaxed text-gray-500 line-clamp-3"></p>
-          </div>
-        </article>
-      ))}
-    </>
+          </article>
+        ))}
+      </div>
+      <div className="carousel-controls">
+        <button
+          onClick={prevSlide}
+          className="carousel-control-prev space-x-4 text-white"
+        >
+          Prev
+        </button>
+
+        <button
+          onClick={nextSlide}
+          className="carousel-control-next space-x-4 text-white"
+        >
+          Next
+        </button>
+      </div>
+    </div>
   );
 };
-
 export default function LandingPage() {
+  const dummyData: IArticle[] = [
+    {
+      id: 0,
+      title: "1",
+      description: "soy un texto de prueba",
+      createdBy: "Edgar",
+      image:
+        "https://yca.org.ar/wp-content/uploads/sites/4/2019/06/perfil-avatar-hombre-icono-redondo_24640-14044.jpg",
+    },
+    {
+      id: 1,
+      title: "2",
+      description: "soy un texto de prueba dos",
+      createdBy: "Edgar",
+      image:
+        "https://yca.org.ar/wp-content/uploads/sites/4/2019/06/perfil-avatar-hombre-icono-redondo_24640-14044.jpg",
+    },
+    {
+      id: 2,
+      title: "3",
+      description: "soy un texto de prueba 3s",
+      createdBy: "IVAN",
+      image:
+        "https://yca.org.ar/wp-content/uploads/sites/4/2019/06/perfil-avatar-hombre-icono-redondo_24640-14044.jpg",
+    },
+    {
+      id: 3,
+      title: "4",
+      description: "soy un texto de prueba dos",
+      createdBy: "Edgar",
+      image:
+        "https://yca.org.ar/wp-content/uploads/sites/4/2019/06/perfil-avatar-hombre-icono-redondo_24640-14044.jpg",
+    },
+    {
+      id: 4,
+      title: "5",
+      description: "soy un texto de prueba 3s",
+      createdBy: "IVAN",
+      image:
+        "https://yca.org.ar/wp-content/uploads/sites/4/2019/06/perfil-avatar-hombre-icono-redondo_24640-14044.jpg",
+    },
+    {
+      id: 5,
+      title: "6",
+      description: "soy un texto de prueba dos",
+      createdBy: "Edgar",
+      image:
+        "https://yca.org.ar/wp-content/uploads/sites/4/2019/06/perfil-avatar-hombre-icono-redondo_24640-14044.jpg",
+    },
+    {
+      id: 6,
+      title: "7",
+      description: "soy un texto de prueba 3s",
+      createdBy: "IVAN",
+      image:
+        "https://yca.org.ar/wp-content/uploads/sites/4/2019/06/perfil-avatar-hombre-icono-redondo_24640-14044.jpg",
+    },
+    {
+      id: 7,
+      title: "8",
+      description: "soy un texto de prueba dos",
+      createdBy: "Edgar",
+      image:
+        "https://yca.org.ar/wp-content/uploads/sites/4/2019/06/perfil-avatar-hombre-icono-redondo_24640-14044.jpg",
+    },
+    {
+      id: 8,
+      title: "9",
+      description: "soy un texto de prueba 3s",
+      createdBy: "IVAN",
+      image:
+        "https://yca.org.ar/wp-content/uploads/sites/4/2019/06/perfil-avatar-hombre-icono-redondo_24640-14044.jpg",
+    },
+    {
+      id: 9,
+      title: "10",
+      description: "soy un texto de prueba dos",
+      createdBy: "Edgar",
+      image:
+        "https://yca.org.ar/wp-content/uploads/sites/4/2019/06/perfil-avatar-hombre-icono-redondo_24640-14044.jpg",
+    },
+    {
+      id: 10,
+      title: "11",
+      description: "soy un texto de prueba 3s",
+      createdBy: "IVAN",
+      image:
+        "https://yca.org.ar/wp-content/uploads/sites/4/2019/06/perfil-avatar-hombre-icono-redondo_24640-14044.jpg",
+    },
+  ];
+
   return (
     <>
       <div className="bg-gray-900 ">
-        <div className="mx-auto max-w-3xl">
-          <div className="mx-auto max-w-screen-xl px-60 py-32 lg:flex lg:h-screen">
+        <div className="mx-auto max-w-screen px-4 py-32 lg:flex lg:h-screen lg:items-center">
+          <div className="mx-auto max-w-3xl text-center">
             <div className="mx-auto">
-              <h1 className="mt-6 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text  font-extrabold text-transparent text-3xl sm:text-5xl ">
+              <h1 className="pb-8 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
                 Retro Wiki
               </h1>
-              <div className="w-full max-w-md px-8 py-4 mt-16 bg-white rounded-lg shadow-lg dark:bg-gray-800">
-                <div className="flex justify-center -mt-16 md:justify-end">
-                  <img
-                    className="object-cover w-20 h-20 border-2 border-blue-500 rounded-full dark:border-blue-400"
-                    alt="Testimonial avatar"
-                    src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80"
-                  ></img>
-                </div>
-
-                <h2 className="mt-2 text-xl font-semibold text-gray-800 dark:text-white md:mt-0">
-                  Design Tools
-                </h2>
-
-                <p className="mt-2 text-sm text-gray-600 dark:text-gray-200">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-                  dolores deserunt ea doloremque natus error, rerum quas odio
-                  quaerat nam ex commodi hic, suscipit in a veritatis pariatur
-                  minus consequuntur!
-                </p>
-
-                <div className="flex justify-end mt-4">
-                  <a
-                    href="#"
-                    className="text-lg font-medium text-blue-600 dark:text-blue-300"
-                    tabIndex={0}
-                    role="link"
-                  >
-                    John Doe
-                  </a>
-                </div>
-              </div>
-
-              <div className="flex justify-center mb-24 py-32 lg:flex lg:h-20 lg:items-center gap-28 ">
-                <DummyComponent data={dummyData} />
-              </div>
+              <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed text-white">
+                Retro Wiki es una enciclopedia virtual que contiene información sobre videojuegos retro, consolas, accesorios y mucho más. 
+    
+              </p>
             </div>
           </div>
-          {/* Use the DummyComponent to render the dummy data */}
+        </div>
+        <div className=" mx-auto max-w-screen px-4 py-32 lg:flex lg:h-screen lg:items-center">
+          <div className="mx-auto max-w-3xl text-center">
+            <h1 className="pb-8 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
+              Conoce nuestras ultimas noticias
+            </h1>
+            <DummyComponent data={dummyData} />
+          </div>
         </div>
       </div>
     </>
